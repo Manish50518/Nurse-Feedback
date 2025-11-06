40d889bf2d09e77e1fcf2409b1137bf03dccfd32
Manish <156883335+Manish50518@users.noreply.github.com>
2025-11-06 13:30:54 +0530

    feat:inital commit

--- components/feedback/FeedbackDetailCards.tsx
+++ components/feedback/FeedbackDetailCards.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useFeedbackStore } from "@/app/Store/useFeedbackStore";
 import { useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { BarChart3, Users, TrendingUp } from "lucide-react";
+import { useFeedbackStore } from "@/app/Store/useFeedbackStore";
 
 function FeedbackDetailCards() {
   const technicalFeedback = useFeedbackStore(
